8557c47439f85f784d8b4c9b31930c18a5f9af29
rizwanptiinfotech <rizwanp@tiinfotech.com>
2023-01-23 12:59:53 +0530

    dewdewdew

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,6 +1,6 @@
 //import React from 'react';
 import React, {useState} from 'react';
-//import Chart from "chart.js/auto";
+import Chart from "chart.js/auto";
 
 import { Line } from "react-chartjs-2";
 import { Bar } from "react-chartjs-2";
